ccbf7ec885c02c85ce00ba24f63e9154a4689641
unknown <krishna6240@gmail.com>
2021-11-11 13:16:46 +0000

    This is my Project work

---
+++ src/KP_Project1.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class KP_Project1 {
+    public static void main(String[] args) {
+        String baseUrl = "https://www.google.co.uk";
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get(baseUrl);
+        driver.manage().window().maximize();//maximise
+        String getTitle = driver.getTitle();
+        System.out.println(getTitle);
+        driver.close();
+
+    }
+}
